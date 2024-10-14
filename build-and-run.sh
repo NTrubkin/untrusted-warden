3d1bef916fe586e43adf03e2d93d2a416cb20a34
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-10-15 00:48:16 +0300

    Добавлено readme.md

--- build-and-run.sh
+++
@@ -1,4 +0,0 @@
-mvn clean install -f demo-server/pom.xml
-mvn clean install -f demo-client/pom.xml
-mvn clean install -f demo/pom.xml
-java -jar demo/target/demo-1.0-SNAPSHOT-jar-with-dependencies.jar
--- run.sh
+++
@@ -1,1 +0,0 @@
-java -jar demo/target/demo-1.0-SNAPSHOT-jar-with-dependencies.jar
